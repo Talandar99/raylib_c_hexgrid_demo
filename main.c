2c7bc9be70916501e7676e035860253b2e5fd333
talandar99 <talandar66@protonmail.com>
2023-11-16 13:47:56 +0100

    feat: mouse location detection

--- main.c
+++ main.c
@@ -11,14 +11,18 @@ void DrawHexagon(double radius,double width,double height){
     float vert = (3.0/2.0) * radius;
     double x=0,y=0;
 
+    Vector2 mouse_position = GetMousePosition();
+
     for (double y=0; y<height; y++) {
         for (double x=-(int)(y*0.5); x<width-(int)(y*0.5); x++) {
-            //printf("y = %f x = %f \n", y,x);
             double hex_x=(horiz*(x+(y*0.5)))+offset_x;
             double hex_y=(vert*y)+offset_y;
-
-            DrawPoly((Vector2){hex_x, hex_y}, 6, radius, 0, LIGHTGRAY);
-            DrawPolyLinesEx((Vector2){hex_x, hex_y}, 6, radius, 0, 2, BLACK);
+            double mouse_hex_center_distance = sqrt(pow(hex_x-mouse_position.x,2)+pow(hex_y-mouse_position.y,2));
+            if (mouse_hex_center_distance<(radius*0.8)) {
+                DrawPoly((Vector2){hex_x, hex_y}, 6, radius, 0, GRAY);
+            }else{
+                DrawPoly((Vector2){hex_x, hex_y}, 6, radius, 0, LIGHTGRAY);
+            }
             DrawText(TextFormat("x: %d", (int)x), hex_x-(radius/2), hex_y, 20, RED);
             DrawText(TextFormat("y: %d", (int)y), hex_x-(radius/2), hex_y-(radius/2), 20, BLUE);
         }
@@ -31,12 +35,11 @@ int main(void){
     //720p 
     const int screenWidth = 1280;
     const int screenHeight = 720;
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "hex grid demo");
     SetTargetFPS(60);           
     Vector2 ballPosition = { -100.0f, -100.0f };
     Color ballColor = DARKBLUE;
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
+    while (!WindowShouldClose()){
         // Update
         //----------------------------------------------------------------------------------
         ballPosition = GetMousePosition();
