afa4838ffa7b9da8c045622cf7ddd39fbe8c2b6e
Talandar <talandar66@protonmail.com>
2023-12-25 20:54:30 +0100

    feat: finished example

--- main.c
+++ main.c
@@ -2,35 +2,52 @@
 #include "raylib.h"
 #include "stdlib.h"
 
-struct HexGridTile{
+struct GridTile{
     Vector2 screen_coordinates;
-    double radius;
     bool is_visible;
 };
 
-void DrawHexagon(double radius, double width, double height,struct HexGridTile** grid) {
-    // first line
+void GenerateHexGrid(double radius, double width, double height,struct GridTile** grid) {
     double offset_x = 100;
     double offset_y = 100;
     float horiz = sqrtf(3) * radius;
     float vert = (3.0 / 2.0) * radius;
-    double x = 0, y = 0;
-
-    Vector2 mouse_position = GetMousePosition();
 
     for (int y = 0; y < height; y++) {
         for (int x = -(int)(y * 0.5); x < width - (int)(y * 0.5); x++) {
             double hex_x = (horiz * (x + (y * 0.5))) + offset_x;
             double hex_y = (vert * y) + offset_y;
-            double mouse_hex_center_distance =
-                sqrt(pow(hex_x - mouse_position.x, 2) +
-                     pow(hex_y - mouse_position.y, 2));
+            grid[y][x].is_visible=true;
+            grid[y][x].screen_coordinates.x=hex_x;
+            grid[y][x].screen_coordinates.y=hex_y;
+        }
+    }
+};
 
-            if (mouse_hex_center_distance < (radius * 0.8)) {
-                DrawPoly((Vector2){hex_x, hex_y}, 6, radius, 90, GRAY);
-            } else {
-                DrawPoly((Vector2){hex_x, hex_y}, 6, radius, 90, LIGHTGRAY);
-            }
+
+void DrawHexGrid(double radius, double width, double height,struct GridTile** grid) {
+    for (int y = 0; y < height; y++) {
+        for (int x = -(int)(y * 0.5); x < width - (int)(y * 0.5); x++) {
+            DrawPoly((Vector2){grid[y][x].screen_coordinates.x, grid[y][x].screen_coordinates.y}, 6, radius, 90, LIGHTGRAY);
+        }
+    }
+};
+
+void DrawHexGridOutline(double radius, double width, double height,struct GridTile** grid) {
+    for (int y = 0; y < height; y++) {
+        for (int x = -(int)(y * 0.5); x < width - (int)(y * 0.5); x++) {
+            double hex_x = grid[y][x].screen_coordinates.x;
+            double hex_y = grid[y][x].screen_coordinates.y;
+            DrawPolyLines((Vector2){hex_x, hex_y}, 6, radius, 90, BLACK);
+        }
+    }
+};
+
+void DrawDebugCoordinatesOnHexGrid(double radius, double width, double height,struct GridTile** grid) {
+    for (int y = 0; y < height; y++) {
+        for (int x = -(int)(y * 0.5); x < width - (int)(y * 0.5); x++) {
+            double hex_x = grid[y][x].screen_coordinates.x;
+            double hex_y = grid[y][x].screen_coordinates.y;
             DrawText(TextFormat("x: %d", (int)x + (int)(height / 2)),
                      hex_x - (radius / 2), hex_y, 20, RED);
             DrawText(TextFormat("y: %d", (int)y), hex_x - (radius / 2),
@@ -39,6 +56,21 @@ void DrawHexagon(double radius, double width, double height,struct HexGridTile**
     }
 };
 
+void DrawMouseOnGrid(double radius, double width, double height,struct GridTile** grid) {
+    Vector2 mouse_position = GetMousePosition();
+    for (int y = 0; y < height; y++) {
+        for (int x = -(int)(y * 0.5); x < width - (int)(y * 0.5); x++) {
+            double hex_x = grid[y][x].screen_coordinates.x;
+            double hex_y = grid[y][x].screen_coordinates.y;
+            double mouse_hex_center_distance =
+                sqrt(pow(hex_x - mouse_position.x, 2) +
+                     pow(hex_y - mouse_position.y, 2));
+            if (mouse_hex_center_distance < (radius * 0.8)) {
+                DrawPoly((Vector2){hex_x, hex_y}, 6, radius, 90, GRAY);
+            } 
+        }
+    }
+};
 int main(void) {
     // Initialization
     //--------------------------------------------------------------------------------------
@@ -49,21 +81,26 @@ int main(void) {
     int height = 9;
     int grid_width = width+(height / 2);
     int grid_height = height;
-
-    struct HexGridTile** grid =(struct HexGridTile**) malloc(grid_width * sizeof(struct HexGridTile*));
-    for (int i = 0; i < grid_width; i++) {
-        grid[i] = (struct HexGridTile*)malloc(grid_height * sizeof(struct HexGridTile));
+    double radius = 40;
+    //memory allocation
+    ///////////////////////////////////////////////////////
+    struct GridTile** grid =(struct GridTile**) malloc(grid_height * sizeof(struct GridTile*));
+    for (int x = 0; x < grid_height; x++) {
+        grid[x] = (struct GridTile*)malloc(grid_width * sizeof(struct GridTile));
     }
-
-    for (int x=0; x<grid_width; x++) {
-        for (int y=0; y<grid_height; y++) {
-            grid[x][y].is_visible=false;
+    ///////////////////////////////////////////////////////
+    for (int y=0; y<grid_height; y++) {
+        for (int x=0; x<grid_width; x++) {
+            grid[y][x].is_visible=false;
         }
     }
+
     InitWindow(screenWidth, screenHeight, "hex grid demo");
     SetTargetFPS(60);
+
     Vector2 ballPosition = {-100.0f, -100.0f};
     Color ballColor = DARKBLUE;
+
     while (!WindowShouldClose()) {
         // Update
         //----------------------------------------------------------------------------------
@@ -86,9 +123,11 @@ int main(void) {
         //----------------------------------------------------------------------------------
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        double radius = 40.;
-
-        DrawHexagon(radius, width, height, grid);
+        GenerateHexGrid(radius, width, height, grid);
+        DrawHexGrid(radius, width, height, grid);
+        DrawMouseOnGrid(radius, width, height, grid);
+        DrawHexGridOutline(radius, width, height, grid);
+        DrawDebugCoordinatesOnHexGrid(radius, width, height, grid);
         DrawCircleV(ballPosition, 10, ballColor);
         EndDrawing();
     }
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm a.out
-cc main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+gcc main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 ./a.out
 rm a.out
